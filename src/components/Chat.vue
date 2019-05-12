37ddbab22940658f8609202793ddb6f98b7246de
suyashsonawane <suyashsonawane007@gmail.com>
2019-05-13 00:56:14 +0530

    first

---
+++ src/components/Chat.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="chat container" >
+    <h2 class="center teal-text">Dev Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="gray-text text-darken-3">message</span>
+            <span class="gray-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text">
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Chat',
+    props:['name'],
+    data(){
+      return{
+        
+      }
+
+    },
+    methods:{
+        
+    }
+
+}
+</script>
+
+<style>
+.chat h2{
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span{
+  font-size: 1.4em;
+}
+.chat .time{
+  display: block;
+  font-size: 1.2em
+}
+</style>
--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,7 @@
 import firebase from 'firebase'
  // Your web app's Firebase configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyDAVA9sekNAp543g6UFz0LCnvnS27wuZHs",
-    authDomain: "geo-devs-b3076.firebaseapp.com",
-    databaseURL: "https://geo-devs-b3076.firebaseio.com",
-    projectId: "geo-devs-b3076",
-    storageBucket: "geo-devs-b3076.appspot.com",
-    messagingSenderId: "799979772173",
-    appId: "1:799979772173:web:0a12cac95edd95de"
+    // keys
   };
   // Initialize Firebase
   const firebaseapp= firebase.initializeApp(firebaseConfig);
